2d2ffad52acb0c8eb37a126a0e54a2e079bc9d53
msrinag <51700595+msrinag@users.noreply.github.com>
2024-10-04 00:18:36 -0400

    Update portfolio.py

--- portfolio.py
+++ portfolio.py
@@ -1,26 +1,23 @@
 import pandas as pd
+import chromadb
 import uuid
-from langchain.vectorstores import Chroma
+import streamlit as st
+from streamlit_chromadb_connection.chromadb_connection import ChromadbConnection
 
 class Portfolio:
     def __init__(self, file_path="my_portfolio.csv"):
         self.file_path = file_path
         self.data = pd.read_csv(file_path)
-        # Initialize Chroma with persistent storage
-        self.chroma_client = Chroma(persist_directory="vectorstore")
-        self.collection = self.chroma_client
+        configuration = {"client": "PersistentClient", "path": "/tmp/.chroma"}
+        self.conn = st.connection("chromadb", type=ChromadbConnection, **configuration)
+        self.collection = self.conn.get_or_create_collection(name="portfolio")
 
     def load_portfolio(self):
-        if not self.collection._collection.count():
+        if not self.collection.count():
             for _, row in self.data.iterrows():
-                self.collection.add_texts(
-                    texts=[row["Techstack"]],
-                    metadatas=[{"links": row["Links"]}],
-                    ids=[str(uuid.uuid4())]
-                )
-            self.collection.persist()
+                self.collection.add(documents=row["Techstack"],
+                                    metadatas={"links": row["Links"]},
+                                    ids=[str(uuid.uuid4())])
 
     def query_links(self, skills):
-        # Mimicking query behavior exactly
-        results = self.collection._collection.query(query_texts=[skills], n_results=2)
-        return results['metadatas']
+        return self.collection.query(query_texts=skills, n_results=2).get('metadatas', [])
