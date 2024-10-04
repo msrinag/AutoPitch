ea2c67e5249a1c004e79c5431d33d5317d8cc8ac
msrinag <51700595+msrinag@users.noreply.github.com>
2024-10-04 01:25:58 -0400

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from utils import clean_text
 
 def create_streamlit_app(llm, portfolio, clean_text):
     st.title("📧 Cold Mail Generator")
-    url_input = st.text_input("Enter a URL:", value="https://jobs.nike.com/job/R-40001")
+    url_input = st.text_input("Enter a URL:", value="https://jobs.nike.com/job/R-40387")
     submit_button = st.button("Submit")
     
     if submit_button:
